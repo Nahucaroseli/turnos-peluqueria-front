7687915f7bf30fbd40197b6580ef734c2da5f30d
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-02-20 13:45:14 -0300

    modificado version eslint y agregado router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './App.css'
-import { Aside } from './components/Aside'
-import { Navbar } from './components/Navbar'
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
 
   return (
-    <div className='min-h-screen'>
-            <Navbar></Navbar>
-      <Aside></Aside>
-
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
