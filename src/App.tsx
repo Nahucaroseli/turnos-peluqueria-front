9c849aa174e4e2a54ae4b885763acedb157011e9
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-02-19 22:50:20 -0300

    agregado navbar

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Navbar } from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className=''>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+export function Navbar(){
+    return(
+
+        <div className="flex flex-row w-screen h-15 bg-amber-200 p-3 gap-x-3">
+            <i className="fa-solid fa-scissors text-4xl"></i>
+            <h1 className="text-2xl mt-1">Peluqueria Vimix</h1>
+        </div>
+    )
+}
+
+
