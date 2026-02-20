6dc8dbfd91096f8a4427ba4dbc66b8917d0c144d
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-02-20 13:21:32 -0300

    agregado iconos

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,7 +1,17 @@
 export function Aside(){
     return (
 
-        <div className="flex flex-col h-screen w-20 bg-amber-200 ">
+        <div className="flex flex-col items-center h-screen w-16 bg-amber-200 pt-40 gap-y-10">
+
+            <i className="fa-solid fa-house text-xl cursor-pointer hover:scale-120 transition"></i>
+            
+            <i className="fa-solid fa-calendar-days text-xl cursor-pointer hover:scale-120 transition"></i>
+
+            <i className="fa-solid fa-scissors text-xl cursor-pointer hover:scale-120 transition"></i>
+            
+            <i className="fa-solid fa-chart-line text-xl cursor-pointer hover:scale-120 transition"></i>
+            
+            <i className="fa-solid fa-gear text-xl cursor-pointer hover:scale-120 transition mt-auto mb-6"></i>
 
         </div>
 
