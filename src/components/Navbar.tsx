c9721571d11eafaffd69d25fcc87d6394254e189
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-02-20 18:20:35 -0300

    corregido repeticion de codigo menu burger

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,9 +12,8 @@ export function Navbar(){
             <h1 className="text-2xl mt-3">Peluqueria Vimix</h1>
             <i onClick={(e)=>{e.preventDefault(); setIsOpen(!isOpen);}} className="absolute right-3 fa-solid cursor-pointer fa-bars text-2xl mt-4 md:invisible"></i>
         </div>
-           
-        {!isOpen ? (
-        <div className="fixed transform translate-x-full transition duration-800 ease-in-out flex flex-col gap-y-20 w-full bg-amber-200 h-screen pt-30 items-center gap-x-5">
+        
+        <div className={`fixed ${isOpen? "translate-x-0":"translate-x-full"} transform transition duration-800 ease-in-out flex flex-col gap-y-20 w-full bg-amber-200 h-screen pt-30 items-center gap-x-5`}>
             <Link to={"/"} className="text-xl cursor-pointer hover:scale-120 transition">Home</Link >
             
             <Link to={"/calendario"} className=" text-xl cursor-pointer hover:scale-120 transition">Calendario</Link >
@@ -25,19 +24,7 @@ export function Navbar(){
             
             <Link to={"/configuracion"} className=" text-xl cursor-pointer hover:scale-120 transition mb-6">Configuracion</Link >
         </div>
-        ):
-        <div className="fixed transform translate-x-0 transition duration-800 ease-in-out flex flex-col gap-y-20 w-full bg-amber-200 h-screen pt-30 items-center gap-x-5">
-             <Link to={"/"} className="text-xl cursor-pointer hover:scale-120 transition">Home</Link >
-            
-            <Link to={"/calendario"} className=" text-xl cursor-pointer hover:scale-120 transition">Calendario</Link >
 
-            <Link to={"/servicios"} className=" text-xl cursor-pointer hover:scale-120 transition">Servicios</Link >
-            
-            <Link to={"/estadisticas"} className=" text-xl cursor-pointer hover:scale-120 transition">Estadisticas</Link >
-            
-            <Link to={"/configuracion"} className=" text-xl cursor-pointer hover:scale-120 transition mb-6">Configuracion</Link >
-        </div>
-        }
         </>
     )
 }
