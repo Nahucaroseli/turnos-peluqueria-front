a745605c072bb01ac4017615add9741015f51c2f
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-02-23 14:46:18 -0300

    agregado api de backend deployed

--- src/api/service-clients.tsx
+++ src/api/service-clients.tsx
@@ -1,9 +1,10 @@
 import type { Client } from "../types/Client";
 
-const LOCAL_API = import.meta.env.VITE_LOCAL_API;
+//const LOCAL_API = import.meta.env.VITE_LOCAL_API;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getClients = async():Promise<Client[]> =>{
-    const response = await fetch(`${LOCAL_API}/clients`);
+    const response = await fetch(`${API_URL}/clients`);
     const data = await response.json();
     return data;
 };
