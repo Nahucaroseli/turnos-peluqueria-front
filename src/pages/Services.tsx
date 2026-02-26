af3922e5bc2913b2dcdde1a6b760f31e692462b0
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-02-26 16:17:06 -0300

    finalizado form para service

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Aside } from "../components/Aside"
 import { Navbar } from "../components/Navbar"
 import { ServiceContext } from "../context/ServiceContext"
@@ -8,11 +8,12 @@ export function Services(){
 
     const {services} = useContext(ServiceContext)!;
 
+    const [openServiceForm,setOpenServiceForm] = useState(false);
     
     useEffect(()=>{
         
         console.log(services);
-    })
+    },[services])
 
 
     return (
@@ -24,6 +25,7 @@ export function Services(){
                     <Aside></Aside>
                     <main className="flex-1 overflow-y-auto p-6 mt-20">
                             <h1 className="text-2xl">Servicios</h1>
+                            <button onClick={(e)=> setOpenServiceForm(!openServiceForm)} className="absolute right-3 top-27 cursor-pointer transition hover:scale-105">Agregar Servicio</button>
                             <div className="flex flex-wrap gap-y-10 gap-x-10 mt-10 w-full md:flex-row">
                                 {services.map((service)=>{
                                     return (
@@ -42,20 +44,25 @@ export function Services(){
 
             </div>
         </div>
-        <ServiceForm></ServiceForm>
+        <ServiceForm formValue={openServiceForm} setFormValue={setOpenServiceForm}></ServiceForm>
         </>
     )
 }
 
-export function ServiceForm(){
+export function ServiceForm({formValue,setFormValue}:{formValue:boolean,setFormValue:React.Dispatch<React.SetStateAction<boolean>>}){
 
     const {register,handleSubmit} = useForm();
 
     return(
-        <form className="flex flex-col fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-100 h-100 bg-white text-center">
+        <form className={`${formValue ? "flex flex-col fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-100 h-100 bg-white text-center rounded-xl":"hidden"}`}>
             <h1 className="mt-10 text-xl">Agregar Servicio</h1>
-            <div className="flex flex-col text-center w-50">
-                <input type="text" {...register("name",{required:true})} placeholder="Nombre del Servicio"/>
+            <i onClick={(e)=> setFormValue(!formValue)} className="fa-solid fa-x relative -top-14 -right-93 cursor-pointer"></i>
+            <div className="flex flex-col items-center text-center w-full mt-10 gap-y-7">
+                <label htmlFor="">Nombre</label>
+                <input type="text" className="border-1 border-solid w-50" {...register("name",{required:true})} placeholder="Nombre del Servicio"/>
+                <label htmlFor="">Precio</label>
+                <input type="text" className="border-1 border-solid w-50" {...register("price",{required:true})} placeholder="Precio"/>
+                <button type="submit" className="cursor-pointer hover:underline hover:scale-110 transition">Agregar</button>
             </div>
         </form>
     )
