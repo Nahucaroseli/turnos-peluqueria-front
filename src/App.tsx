9fb23e6dd53c2b0a9dfe09a7622925729014996c
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-03-07 13:46:53 -0300

    agregado presentacion de turnos y renombrado archivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Calendar } from './pages/Calendar';
+import { Appointments } from './pages/Appointments';
 import { Services } from './pages/Services';
 import { Stats } from './pages/Stats';
 import { Config } from './pages/Config';
 
+
 function App() {
+
+
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/calendario" element={<Calendar />} />
+      <Route path="/calendario" element={<Appointments />} />
       <Route path="/servicios" element={<Services />} />
       <Route path="/estadisticas" element={<Stats />} />
       <Route path="/configuracion" element={<Config />} />
--- src/pages/Appointments.tsx
+++ src/pages/Appointments.tsx
@@ -1,8 +1,17 @@
+import { useContext, useState } from "react";
 import { Aside } from "../components/Aside"
 import { Navbar } from "../components/Navbar"
+import { TurnoContext } from "../context/turno.context";
 
 
-export function Calendar(){
+export function Appointments(){
+
+  const {turnos} = useContext(TurnoContext)!;
+
+  const [openTurnoForm,setOpenTurnoForm] = useState(false);
+
+
+
         return (
                 <div className='h-screen flex flex-col'>
                     <Navbar></Navbar>
@@ -10,6 +19,8 @@ export function Calendar(){
                         <Aside></Aside>
                         <main className="flex-1 overflow-y-auto p-6 mt-20">
                                 <h1 className="text-2xl">Turnos</h1>
+                                <button onClick={()=> setOpenTurnoForm(!openTurnoForm)} className="absolute right-3 top-27 cursor-pointer transition hover:scale-105">Asignar Turno</button>
+                                <div></div>
                         </main>
                     </div>
         
